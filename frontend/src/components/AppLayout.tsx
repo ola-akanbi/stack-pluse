f3ba4903239a3eaf9867c9363ade28f32f38f5b5
ola-usman <ola4usman@gmail.com>
2026-05-13 22:02:16 +0100

    Refactor AppLayout component structure for improved readability

--- frontend/src/components/AppLayout.tsx
+++ frontend/src/components/AppLayout.tsx
@@ -6,32 +6,3 @@ import { PageTransition } from './PageTransition';
 import { usePulseNotifications } from '@/hooks/use-pulse-notifications';
 import { NotificationProvider } from '@/lib/notification-store';
 import { CommandPalette } from './CommandPalette';
-
-export function AppLayout() {
-  const location = useLocation();
-
-  return (
-    <NotificationProvider>
-      <AppLayoutInner location={location} />
-    </NotificationProvider>
-  );
-}
-
-function AppLayoutInner({ location }: { location: ReturnType<typeof useLocation> }) {
-  usePulseNotifications();
-
-  return (
-    <div className="min-h-screen bg-background flex flex-col">
-      <AppNav />
-      <CommandPalette />
-      <main className="mx-auto w-full max-w-7xl flex-1 px-4 py-6">
-        <AnimatePresence mode="wait">
-          <PageTransition key={location.pathname}>
-            <Outlet />
-          </PageTransition>
-        </AnimatePresence>
-      </main>
-      <Footer />
-    </div>
-  );
-}
