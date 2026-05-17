7795f7d9dcf0ed4dd1bbbf92d96fa42dfb3c5e42
ola-akanbi <olamilekanakanbi775@gmail.com>
2026-05-17 18:21:07 +0100

    Add constants for external URLs in Footer component

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,2 +1,6 @@
 import { Zap } from 'lucide-react';
 import { NetworkBadge } from './NetworkBadge';
+
+const EXPLORER_URL = 'https://explorer.hiro.so';
+const DOCS_URL = 'https://docs.stacks.co';
+const GITHUB_URL = 'https://github.com/stacks-network';
